3a2821e7ec14f2b4a817261973a1686069bf5cd7
cybercoder <vahid.lid@live.com>
2024-05-30 12:47:45 +0330

    Add-br

--- pkg/ovsagent/ovsagent.go
+++ pkg/ovsagent/ovsagent.go
@@ -5,23 +5,21 @@ import (
 	"os/exec"
 )
 
-func CreateDistrubutedSwitch(bridge string) {
-	ovsvsctl("create-br", bridge)
+func CreateDistrubutedSwitch(bridge string) (string, error) {
+	output, stdErr := ovsvsctl("add-br", bridge)
+	if stdErr != nil {
+		log.Printf("Error: %v, %v", stdErr,output)
+		return string(output), stdErr
+	}
+	return string(output), nil
 }
 
-func ovsvsctl(params ...string) {
+func ovsvsctl(params ...string) ([]byte, error) {
 	baseArgs := []string{"--db=unix:/host/var/run/openvswitch/db.sock"}
 	args := append(baseArgs, params...)
 
-	command := exec.Command("ovs-vsctl", args...)
-	log.Println(command.String())
+	command := exec.Command("ovs-vsctl",args...)
+	log.Printf("Running: %s \n",command.String())
 
-	// output, err := command.CombinedOutput()
-	// if err != nil {
-	// 	log.Fatalf("%v\n", err)
-	// 	log.Printf("%s\n", output)
-	// 	return
-	// }
-
-	// log.Printf("%s\n", output)
+	return command.CombinedOutput()
 }
